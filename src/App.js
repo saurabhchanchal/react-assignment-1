30ab060c72c91ba4715c947d145a842efe4f12b5
saurabhchanchal <96104633+saurabhchanchal@users.noreply.github.com>
2022-04-08 23:11:49 +0530

    react Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+import "./App.css"
+
+// here always come with capital case with function
 function App() {
+  const OpSystem = ["Android", "Blackberry", "iPhone", "WhindowPhone"];
+  const Manufacturers = [ {name:"Android", style: "square"}, {name: "HTC", style: "square"}, {name: "Micromax", style: "disc"}, {name: "Apple", style: "circle"}]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div className="operating">
+   <h1>Mobile Operating System</h1>
+   {OpSystem.map((ele) => {
+             return <Systeam opsytm={ele}/>
+              // here we take key value pairs
+   })}
+  </div>
+
+<div>
+  <h1>Mobile Manufacturere</h1>
+  {Manufacturers.map((e) => {
+     return <li className= {e.style}>{e.name} </li>
+  })}
+</div>
+
     </div>
   );
+};
+
+//here we are creating Component
+function Systeam(parameter) {
+     return <li className="changesize">{parameter.opsytm }</li>   
 }
 
 export default App;
+//age mai ternery(3)at line 14 operator use hua hai es k ander{}
